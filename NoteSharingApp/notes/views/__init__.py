64787beae9c9e48485bac51f251663958d91122f
cari <anhthuchevu@gmail.com>
2025-07-01 13:43:26 +0700

    fixing code

--- NoteSharingApp/notes/views/__init__.py
+++ NoteSharingApp/notes/views/__init__.py
@@ -1,6 +1,6 @@
 from .createNote import createNoteView
 from .editNote import editNoteView
-from .viewNote import ViewSharedNoteView
+from .viewNote import viewNoteView
 from .shareNote import shareNoteView
 from .deleteNote import deleteNoteView
 from .viewNoteByLink import ByLinkNoteView
